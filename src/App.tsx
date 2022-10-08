746507c2ff2a4dc377ec8602aeac671c6d7ff379
Seulgi <62641007+sssseulg2@users.noreply.github.com>
2022-10-08 16:03:19 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import MainPage from './pages/Main/Main';
 
 function App() {
 
   // 로그인 되어 있ㄹ으면 홈으로 넘기고 안되어있으면 로그인/회원가입(메인)으로
   return (
-    <></>
+    <BrowserRouter> 
+    <Routes>
+      <Route path="/" element= {<MainPage/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function MainPage() {
+    return(
+        <div>어뉴로ㅓ카니머ㅜ퓨ㅗ아니</div>
+    );
+}
+export default MainPage;
