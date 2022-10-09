6745fac4d6102365c27d6e2ef928400e039dc256
Seulgi <62641007+sssseulg2@users.noreply.github.com>
2022-10-09 14:42:20 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import './styles/tailwind.css';
 
 import Main from './pages/Main/Main';
@@ -7,18 +7,17 @@ import Home from './pages/Home/Home';
 import Login from 'pages/Auth/Login';
 
 function App() {
+  const navigate = useNavigate();
 
   return (
     <div className='font-score m-8'>
-      <h1 className='text-mainGreen text-xl font-bold mb-8'>부랴부랴</h1>
+      <h1 className='text-mainGreen text-xl font-bold mb-8' onClick={() => {navigate("/home")}}>부랴부랴</h1>
       
-    <BrowserRouter> 
     <Routes>
       <Route path="/" element= {<Main/>}></Route>
       <Route path="/login" element= {<Login/>}></Route>
       <Route path="/home" element= {<Home/>}></Route>
     </Routes>
-    </BrowserRouter>
     </div>
 
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/tailwind.css';
 
@@ -7,7 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
 );
