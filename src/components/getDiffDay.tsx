501ed755d5bf9443c731b0e52dc69f93a2d6bc3a
jeongje <wjdwp0926@naver.com>
2022-11-18 20:51:50 +0900

    공연이 야속해,,

--- src/components/getDiffDay.tsx
+++ src/components/getDiffDay.tsx
@@ -3,7 +3,7 @@ const getDiffDay = (departureTime: string, arrivalTime: string) => {
   const start = new Date(Number(departureTime)); // 출발시간
   const end = new Date(Number(arrivalTime)); // 도착시간
   const diffDay = ((end.getTime() - start.getTime()) * 1000) / (1000 * 60 * 60 * 24);
-  console.log(diffDay);
+  //console.log(diffDay);
 
   // 당일
   if (Math.floor(diffDay) == 0) {
