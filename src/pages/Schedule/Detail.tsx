9a6ef6e69f335e63854822922d8894c81a7b9fcc
Seulgi <62641007+sssseulg2@users.noreply.github.com>
2022-11-28 16:53:27 +0900

    detail page

--- src/pages/Schedule/Detail.tsx
+++ src/pages/Schedule/Detail.tsx
@@ -5,9 +5,11 @@ import { useParams } from 'react-router-dom';
 import getDate from 'components/getDate';
 import Subway from 'components/Subway';
 import SubwayColor from 'components/SubwayColor';
+import SubwayLine from 'components/SubwayLine';
 
 interface Ischedule {
   id: number;
+  title: string;
   origin: string;
   destination: string;
   originLatitude: string;
@@ -40,6 +42,7 @@ function Detail() {
 
   const [schedule, setSchedule] = React.useState<Ischedule>({
     id: 0,
+    title: '',
     origin: '',
     destination: '',
     originLatitude: '',
@@ -72,15 +75,18 @@ function Detail() {
       });
       setPath(JSON.parse(response.data.data.path));
       setSchedule(response.data.data);
+      console.log(response.data.data);
     } catch (error) {
       console.log(error);
     }
   };
   useEffect(() => {
+
     getSchedule();
   }, []);
   return (
     <div>
+      <div className='mb-3 text-2xl text-lightBlack font-semibold'>{schedule.title}</div>
       <div>
         <p className="text-xl text-lightBlack font-normal mb-5 ">
           <span className=" text-mainGreen  font-medium">{schedule.origin}</span> 에서{' '}
@@ -90,51 +96,92 @@ function Detail() {
           알려드릴게요!
         </p>
       </div>
-      <div className="w-100 h-auto rounded-lg drop-shadow-3xl bg-lightGrey p-5">
-        <div className="flex justify-between  font-light text-lg">
-          <p className="text-lightBlack">
+      <div className="w-100 h-auto mb-10 rounded-lg drop-shadow-3xl bg-lightGrey p-5">
+        <div className="flex justify-between mb-6 text-lg">
+          <p className="text-lightBlack font-medium">
             {path.departure_time.text} ~ {path.arrival_time.text}
           </p>
-          <p className="text-darkGrey">{path.duration.text}</p>
+          <p className="text-darkGrey font-light">{path.duration.text}</p>
         </div>
 
         <div>
-          <div>{schedule.origin}</div>
+          <div className="flex">
+            <div className="flex justify-center items-center bg-mainGreen rounded-full w-7 h-7 text-white text-xs">
+              출발
+            </div>
+            <p className="ml-2 mt-0.5 text-base text-lightBlack font-normal "> {schedule.origin}</p>
+          </div>
           {path.steps.map((step: Istep, key: number) => (
             <div key={key}>
+              {step.travel_mode === 'WALKING' && <div className="bg-darkGrey rounded-full w-0.5 h-6 ml-3 mt-1"> </div>}
               {step.travel_mode === 'WALKING' && (
-                <div className="mt-1 flex">
+                <div className="mt-1 flex ">
                   <div className="bg-darkGrey rounded-full w-7 h-7 text-white">
                     <img className="w-7 h-7" alt="부랴부랴" src="/assets/walk.png" />
                   </div>
-                  걷기
+                  <p className="ml-2 mt-0.5 text-sm text-darkGrey font-light ">{step.duration.text}</p>
+                </div>
+              )}
+              {step.travel_mode === 'WALKING' && <div className="bg-darkGrey rounded-full w-0.5 h-6 ml-3 mt-1"> </div>}
+              {step.travel_mode === 'TRANSIT' && step.transit_details.vehicleType === 'BUS' && (
+                <div className="mt-1 flex">
+                  <img className="w-7 h-7" alt="부랴부랴" src="/assets/bus.png" />
+                  <p className="ml-2 mt-0.5 text-base text-lightBlack font-normal ">
+                    {step.transit_details.departure_stop}
+                  </p>
+                  <p className="ml-2 mt-2 text-xs text-darkGrey font-light">{step.transit_details.headsign}</p>
                 </div>
               )}
               {step.travel_mode === 'TRANSIT' && step.transit_details.vehicleType === 'BUS' && (
-                <div className="mt-1 flex">
-                  <img className="w-7 h-7" alt="부랴부랴" src="/assets/bus.png" /> {step.transit_details.departure_stop}
+                <div className="flex items-center">
+                  <div className="bg-[#379df1] rounded-full w-0.5 h-20 ml-3 mt-1"> </div>
+                  <div>
+                    <p className="ml-5 mt-0.5 text-base text-[#379df1] font-normal">{step.transit_details.number} </p>
+
+                    <p className="ml-5 text-sm text-darkGrey font-light"> {step.duration.text} </p>
+                  </div>
                 </div>
               )}
               {step.travel_mode === 'TRANSIT' && step.transit_details.vehicleType === 'BUS' && (
                 <div className="mt-1 flex">
                   <div className="bg-[#379df1] rounded-full w-7 h-7 text-white"></div>
-                  {step.transit_details.arrival_stop}
+                  <p className="ml-2 mt-0.5 text-base text-lightBlack font-normal ">
+                    {step.transit_details.arrival_stop}
+                  </p>
                 </div>
               )}
               {step.travel_mode === 'TRANSIT' && step.transit_details.vehicleType === 'SUBWAY' && (
                 <div className="mt-1 flex">
                   <Subway {...step.transit_details.number} />
-                  {step.transit_details.departure_stop}
+
+                  <p className="ml-2 mt-0.5 text-base text-lightBlack font-normal ">
+                    {step.transit_details.departure_stop}역
+                  </p>
+                  <p className="ml-2 mt-2 text-xs text-darkGrey font-light">{step.transit_details.headsign}</p>
+                </div>
+              )}
+              {step.travel_mode === 'TRANSIT' && step.transit_details.vehicleType === 'SUBWAY' && (
+                <div className="flex items-center">
+                  <SubwayLine {...step.transit_details.number} />
+                  <p className="ml-5 text-sm text-darkGrey font-light"> {step.duration.text} </p>
                 </div>
               )}
               {step.travel_mode === 'TRANSIT' && step.transit_details.vehicleType === 'SUBWAY' && (
                 <div className="mt-1 flex">
                   <SubwayColor {...step.transit_details.number} />
-                  {step.transit_details.arrival_stop}
+                  <p className="ml-2 mt-0.5 text-base text-lightBlack font-normal ">
+                    {step.transit_details.arrival_stop}역
+                  </p>
                 </div>
               )}
             </div>
           ))}
+          <div className="flex mt-1">
+            <div className="flex justify-center items-center bg-mainGreen rounded-full w-7 h-7 text-white text-xs">
+              도착
+            </div>
+            <p className="ml-2 mt-0.1 text-base text-lightBlack font-normal "> {schedule.destination}</p>
+          </div>
         </div>
       </div>
     </div>
